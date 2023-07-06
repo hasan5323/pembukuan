a2529c39077e5ddbc793dbd7bf1fb223a0276421
hasan5323 <ibrahimhasan002@gmail.com>
2023-07-06 14:47:00 +0700

    separate script

---
+++ submitAlert.js
@@ -0,0 +1,14 @@
+var dataa = document.getElementById("dataa");
+var btn = document.getElementById("submit");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  dataa.style.display = "block";
+}
+span.onclick = function() {
+  dataa.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == dataa) {
+    dataa.style.display = "none";
+  }
+}
