54f237799b9ec081f563a0c55a0b8dc6b9413d07
hasan5323 <ibrahimhasan002@gmail.com>
2023-07-07 18:28:23 +0700

    change but nothing added

--- log.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("formulir");
-form.addEventListener("submit", submitHandler);
-function submitHandler(e) {
-  e.preventDefault();
-  console.log(e.target.elements.nama.value);
-  console.log(e.target.elements.harga.value);
-  console.log(e.target.elements.pengali.value);
-}
